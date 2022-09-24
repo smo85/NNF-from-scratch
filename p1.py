c3246821f970d2952afcd8c719a73d7138d935ae
Sarah <sarah.morris@geappliances.com>
2022-09-24 16:35:02 -0400

    relu

--- p1.py
+++ p1.py
@@ -1,11 +1,14 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(100, 3)
 
+# X = [[1, 2, 3, 2.5],
+#      [2.0, 5.0, -1.0, 2.0],
+#      [-1.5, 2.7, 3.3, -0.8]]
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -14,9 +17,27 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
+class Activation_ReLU:
+    def forward(self, inputs):
+     self.output = np.maximum(0, inputs)
+
+
+layer1 = Layer_Dense(2,5)
+activation = Activation_ReLU()
 
 layer1.forward(X)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation.forward(layer1.output)
+print(activation.output)
+
+
+
+
+'''
+NOTES - Activation functions
+2+ hidden layers to fit non-linear
+why - linear can only do linear problems so need non-linear (ReLU is close to linear, but the less than 0 makes it non-linear)
+sigmoid (between 0 and 1) - has vanishing gradient problem
+ReLU - (between 0 and whatever x is) - no vanishing gradient problem
+    1. faster than sigmoid and simpler
+    2. works well
+'''
