a1c38a993100d848764a7697339f35c03657ddc8
Sarah <sarah.morris@geappliances.com>
2022-09-24 15:51:37 -0400

    simple dense layer class

--- p1.py
+++ p1.py
@@ -1,30 +1,22 @@
 import numpy as np
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
+np.random.seed(0)
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
 
-'''
-NOTES
-batches - gives ability to run in parallel
-        - helps with generalization, show it multiple samples at a time
-            - all data at once can cause over-fitting, so smaller batches are good, 32 to 64
-'''
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)  # structured this way so we don't have to transpose the weights
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+layer1 = Layer_Dense(4,5)
+layer2 = Layer_Dense(5,2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
